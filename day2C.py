4a5ac3cb1d4f1d54baf5f9e4f1da89f230644631
Chastly Regino <chastly_regino@icloud.com>
2024-05-03 14:20:24 -0500

    Day 2 of learning python

---
+++ day2C.py
@@ -0,0 +1,12 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# years = 90 - int(age)
+# weeks = years * 52
+# udemy answer ^
+# Write your code below this line 👇
+
+#4680 weeks for 90 years
+
+weeksLeft = 4680 - (int(age) * 52)
+
+print('You have ' + str(weeksLeft) + ' weeks left.')
---
+++ day2D.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+if number % 2 == 0:
+    print('this is an even number.')
+else:
+    print('This is an odd number.')
